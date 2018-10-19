ed53060860c51950b03602ffbd2013c792b15723
marianasrvg <sierravgmr@gmail.com>
2018-10-18 19:35:04 -0500

    BottomNavigationView

--- app/src/main/java/com/example/mari_/pymescontrol/ActivityMain.java
+++ app/src/main/java/com/example/mari_/pymescontrol/ActivityMain.java
@@ -1,13 +1,24 @@
 package com.example.mari_.pymescontrol;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class ActivityMain extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //mTextMessage =  findViewById(R.id.am_textView);
+        //BottomNavigationView navigation = findViewById(R.id.navigation);
+        //navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
+
 }
